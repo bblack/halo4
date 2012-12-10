0a6c574f1626a4edc36f98e418be80aa8f5ff066
Brian Black <black@gatech.edu>
2012-12-10 12:54:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Halo4::Application.config.secret_token = 'cfa4fac459cebd0650012e7330ab6b6b4335d903bf81723981d12bfce5865e40189f4b6aa077a75ee2ab92257b870d8075ed478b4577a39d29a0ebb07fdc2090'
---
+++ lib/waypoint.rb
@@ -0,0 +1,3 @@
+class Waypoint
+
+end
