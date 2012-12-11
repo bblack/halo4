8e6cf0a73f5c73fbecd0934d5823e76fb49d4c99
Brian Black <black@gatech.edu>
2012-12-11 14:20:54 -0500

    kd chart

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'resque'
 gem 'resque-pool'
 gem 'mongo'
 gem 'bson_ext' # for mongo performance!
+gem 'haml'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,2 +1,15 @@
 class DataController < ApplicationController
+  def kd
+    games = Waypoint::Halo4.db['games']
+      .find({'Players.Gamertag' => params[:gamertag]}, {:limit => 200})
+      .sort(:EndDateUtc)
+      .collect do |g|
+        p = g['Players'].find{|p| p['Gamertag'] == params[:gamertag]}
+        {
+          :x => Time.parse(g['EndDateUtc']).to_i * 1000,
+          :y => p['Kills'] - p['Deaths']
+        }
+      end
+    render :json => games
+  end
 end
