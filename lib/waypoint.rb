ca2ae42e18b655015b9f17b406b07d75db1b250b
Brian Black <black@gatech.edu>
2012-12-12 20:04:35 -0500

    lib/waypoint now only fetches missing games; doesn't re-fetch entire history

--- lib/waypoint.rb
+++ lib/waypoint.rb
@@ -22,7 +22,7 @@ class Waypoint
   end
 
   class Halo4
-    @@token = "v2=MbabGhWmFgUwAR4LXslNTRCBmYeeQNlk5VxJouVXzV3yeCZ5JqMUb6ruTYpnELLw7JptifzvIkEs2EsUsn8sGz8D_s2BpIElbrPxInCFnp4hleIjxVvAcVnc6HZfOAHJB5RT0GIpln_CgfV2kULyuYBpUTKx_6LPWuUvSw3ZJR907uZghBUis5YwTkewbVU8zrF44xiJkRgRQLCAOvcnlTTG3tNLaMCt3WWuRQK9tXzuqw30zSs3MmHhmQNuyGmA5eNSAKC-ugfNAiYjmghaDDod9D0bXK3PTJ-qcOptU23o3z8uCbTgkC04q9wsbJzJL4j1jnh-O3jRpgulRXD9yLxx5Y8fCvpzXwXIDVm4tODldh-gy3LUbWVPc-sONkoislbN7ombi84iln_rQaRA_tIDorr8inc"
+    @@token = "v2=hGx4uNdBB1Tr27hTGzVI-EA49BnkV3VuUvQ9Bqy57XYny9AWsV6fIV41q4k_D2HzAMzyLZHe9apHBemp5VNL7NqiLT1zgbg82IzD-5juRCKJd6NC41u5RnwM1WXzjOeIxI2uZWTpMnMEjjTL-vTsFtWy9OcilXdLIBmKrGfblZAw7raevvB-1w0Nh1gBa57YhYyPHgxT8KvTQntjlV0pe1egn1pB3YcOPvzp07geqHZFlXFYeXm2AoRgXNnON2lxavoo-IYJIgZLTyMUzo__I4oWOkQ0Ml8um0XrNMjEx6PAjFVosLTHVFeRlAYDteza9ivzdv9Kf5lPwLHKzE1iDDC_LA0sQusou0KaBcpWmcGAlZek5flLxMI2S9iv_M0GndSsGSiUjsnHtnQFTCKCHmrs555OWz4"
     @@db = Waypoint.mongo_client.db("halo4-#{Rails.env}")
     
     def self.token
@@ -52,20 +52,23 @@ class Waypoint
           raise StandardError.new("Got status #{res.code}") if not res.success?
           o = JSON.parse(res.body)
           last_index += batch_size # If new games are added while we're doing this, then we get overlap
+          # todo: check exactly which games are not in db and only q up those
           games += o['Games']
           break if o['Games'].count < batch_size
+          break if Waypoint::Halo4.db['games'].find_one({:Id => o['Games'].last['Id']}) # last is earliest
         end
 
         # mongo doesn't allow "$" in key
         games.each {|g| Waypoint.mongoify(g)}
 
         games.each do |g|
-          Waypoint::Halo4.db['games'].update(
-            {:Id => g['Id']},
-            g,
-            {:upsert => true}
-          )
-          Resque.enqueue(Waypoint::Halo4::GameDetailsFetcher, g['Id'])
+          begin
+            Waypoint::Halo4.db['games'].insert(g)
+            Resque.enqueue(Waypoint::Halo4::GameDetailsFetcher, g['Id'])
+          rescue Mongo::OperationFailure => ex
+            # if dup id exists on insert, details fetcher won't be Q'd
+            raise unless ex.error_code == 11000
+          end
         end
 
         games
@@ -76,8 +79,6 @@ class Waypoint
       @queue = :everything
 
       def self.perform(game_id)
-        # break it here if we already have this game?
-        # pass an overwrite flag to force fetching it?
         res = Typhoeus.get(
           "https://stats.svc.halowaypoint.com/en-us/h4/matches/#{game_id}",
           {:headers => {:Accept => 'application/json', 'X-343-Authorization-Spartan' => Waypoint::Halo4.token}}
