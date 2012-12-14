d83d63f16c659e235eda8171a57d8bd4db3d45dc
Brian Black <black@gatech.edu>
2012-12-13 21:04:00 -0500

    DRY default highcharts options; tinkered with stats while thinking about standardizing and DRYing data retrieval

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,85 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+Application = {
+  chartOpts: function(opts) {
+    return $.extend(true, 
+      {
+        chart: {
+          borderRadius: 0,
+          borderColor: '#c0c0c0',
+          borderWidth: 1,
+          zoomType: 'x'
+        },
+        credits: {
+          enabled: false
+        },
+        loading: {
+          style: {
+            position: 'absolute',
+            backgroundColor: 'white',
+            backgroundImage: "url('/assets/chart-loading.gif')",
+            backgroundPosition: "center center",
+            backgroundRepeat: "no-repeat",
+            opacity: 0.5,
+            textAlign: 'center'
+          }
+        },
+        plotOptions: {
+          line: {
+            lineWidth: 2,
+            shadow: false,
+            marker: {
+              enabled: false
+            }
+          },
+          series: {
+            marker: {
+              states: {
+                hover: {
+                  enabled: false
+                }
+              }
+            }
+          }
+        },
+        tooltip: {
+          formatter: function() { return new Date(this.x).toString() + '<br/>' + this.y.toString(); }
+        },
+        xAxis: {
+          type: 'datetime'
+        },
+        yAxis: [
+          {
+            title: {
+              text: null,
+              style: {
+                color: '#4572a7'
+              }
+            },
+            labels: {
+              style: {
+                color: '#4572a7'
+              }
+            }
+          },
+          {
+            title: {
+              text: null,
+              style: {
+                color: '#aa4643'
+              }
+            },
+            labels: {
+              style: {
+                color: '#aa4643'
+              }
+            },
+            opposite: true
+          }
+        ],
+      },
+      opts);
+  }
+};
--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -8,33 +8,46 @@ class DataController < ApplicationController
       p = g['Players'].find{|p| p['Gamertag'] =~ gamertagregex}
       {
         :x => Time.parse(g['EndDateUtc']).to_i * 1000,
-        :y => p['Kills'] - p['Deaths']
+        :y => p['Kills'] - p['Deaths'],
+        :marker => {:radius => [[p['Kills'] + p['Deaths'], 0].max, 100].min * (0.1) + 1 }
       }
     end
-    runningspread = 0
-    runningspread = games.collect do |g|
-      runningspread += g[:y]
-      {:x => g[:x], :y => runningspread}
+
+    cumulative = 0
+    data_spread = games.collect do |g|
+      cumulative += g[:y]
+      {:x => g[:x], :y => cumulative}
     end
+
     render :json => [
       {:name => 'Spread by match', :data => games, :type => 'scatter'},
-      {:name => 'Cumulative spread', :data => runningspread, :type => 'line', :yAxis => 1}]
+      {:name => 'Spread (cumulative)', :data => data_spread, :type => 'line', :yAxis => 1}]
   end
 
   def kdr
-    totalk = 0
-    totald = 0
     games = @games.collect do |g|
       p = g['Players'].find{|p| p['Gamertag'] =~ gamertagregex}
-      totalk += p['Kills']
-      totald += p['Deaths']
       {
         :x => Time.parse(g['EndDateUtc']).to_i * 1000,
-        :y => totald == 0 ? 0 : (totalk.to_f / totald)
+        :y => p['Deaths'] == 0 ? -1 : p['Kills'].to_f / p['Deaths'], # TODO: fix div by zero hack
+        :marker => {:radius => [[p['Kills'] + p['Deaths'], 0].max, 100].min * (0.1) + 1 }
       }
     end
 
-    render :json => games
+    cumulative = {k: 0, d: 0}
+    data = @games.collect do |g|
+      p = g['Players'].find{|p| p['Gamertag'] =~ gamertagregex}
+      cumulative[:k] += p['Kills']
+      cumulative[:d] += p['Deaths']
+      {
+        :x => Time.parse(g['EndDateUtc']).to_i * 1000,
+        :y => cumulative[:d] == 0 ? nil : (cumulative[:k].to_f / cumulative[:d])
+      }
+    end
+
+    render :json => [
+      {:name => "KDR by match", :data => games, :type => 'scatter'},
+      {:name => "KDR (cumulative)", :data => data, :type => 'line', :yAxis => 1}]
   end
 
 end
