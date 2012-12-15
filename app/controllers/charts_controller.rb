5f1579cf98301c1961452a66c46d1cd0a14988b3
Brian Black <black@gatech.edu>
2012-12-15 14:10:58 -0500

    dry up chart markup

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,2 +1,23 @@
 class ChartsController < ApplicationController
+  def kdr
+    render(
+      '_chart',
+      :locals => {
+        :element_id => "kdrchart",
+        :data_url => "/data/kdr/#{params[:gamertag]}?#{params.slice(:split).to_query}",
+        :title => "KDR: #{params[:gamertag]}"
+      }
+    )
+  end
+
+  def kd
+    render(
+      '_chart',
+      :locals => {
+        :element_id => "kdchart",
+        :data_url => "/data/kd/#{params[:gamertag]}?#{params.slice(:split).to_query}",
+        :title => "K-D Spread: #{params[:gamertag]}"
+      }
+    )
+  end
 end
